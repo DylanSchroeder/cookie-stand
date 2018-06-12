0ed874adc204fa511026d6789bb6491984cdd8e0
Dylan Schroeder <schrodac@gmail.com>
2018-06-12 13:28:06 -0500

    added ojects for all five locations and put the names of the locations in a list on sales.html

--- JS/app.js
+++ JS/app.js
@@ -1,19 +1,64 @@
 'use strict';
 
 
-var pike = {
+var clarence = {
   minCustomer: 23,
   maxCustomer: 65,
   avgCustomer: 6.3,
 
-  getRandomCustomersPerHour: function(){
+  getCustomersPerHour: function(){
+    return this.minCustomer + Math.random * (this.maxCustomer - this.minCustomer);
 
   },
 
 };
-function getRandomCustomersPerHour (){
-  return this.minCustomer + Math.random * (this.maxCustomer - this.minCustomer);
+clarence.getCustomersPerHour();
 
-}
+var lowden = {
+  minCustomer: 3,
+  maxCustomer: 24,
+  avgCustomer: 1.2,
 
-pike.getRandomCustomersPerHour();
+  getCustomersPerHour: function(){
+    return this.minCustomer + Math.random * (this.maxCustomer - this.minCustomer);
+
+  },
+};
+lowden.getCustomersPerHour();
+
+
+var stanwood = {
+  minCustomer: 11,
+  maxCustomer: 38,
+  avgCustomer: 3.7,
+
+  getCustomersPerHour: function(){
+    return this.minCustomer + Math.random * (this.maxCustomer - this.minCustomer);
+
+  },
+};
+stanwood.getCustomersPerHour();
+
+var mechanicsville = {
+  minCustomer: 20,
+  maxCustomer: 38,
+  avgCustomer: 2.3,
+
+  getCustomersPerHour: function(){
+    return this.minCustomer + Math.random * (this.maxCustomer - this.minCustomer);
+
+  },
+};
+mechanicsville.getCustomersPerHour();
+
+var tipton = {
+  minCustomer: 3,
+  maxCustomer: 24,
+  avgCustomer: 1.2,
+
+  getCustomersPerHour: function(){
+    return this.minCustomer + Math.random * (this.maxCustomer - this.minCustomer);
+
+  },
+};
+tipton.getCustomersPerHour();
