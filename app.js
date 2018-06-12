a5d73890e56f80d0b227b7811edac6a31b4d89ad
Dylan Schroeder <schrodac@gmail.com>
2018-06-12 09:02:26 -0500

    started with object modeling

--- app.js
+++ app.js
@@ -2,10 +2,21 @@
 
 
 var pike = {
-  min: '23',
-  max: '65',
-  avg: '6.3',
+  minCust: 23,
+  maxCust: 65,
+  avgCust: 6.3,
+
+  getRandomCustomersPerHour: function(){
+
+  },
 
 };
+function getRandomCustomersPerHour (){
+  /* minCust = Math.ceil(minCust);
+  maxCust = Math.floor(maxCust);
+  return Math.floor(Math.random() * (this.max - this.min) + min); */
+  return pike.minCust + Math.random * (this.maxCust - this.minCust);
 
-pike(Math.random());
+}
+
+pike.getRandomCustomersPerHour();
